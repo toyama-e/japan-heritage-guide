76bae9ca805db404da4f8f25f268747c6b8d72d3
dlvvlb <dlv.vlb8@gmail.com>
2025-12-29 06:07:01 +0900

    feat: add signup signin

---
+++ frontend/app/auth/login/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import AuthForm from '../../../components/auth/authform';
+import { signIn } from '../../../lib/auth/signin';
+import { useRouter } from 'next/navigation';
+
+export default function LoginPage() {
+  const router = useRouter();
+
+  const handleSignIn = async (email: string, password: string) => {
+    await signIn(email, password);
+    router.replace('/');
+  };
+
+  return (
+    <div>
+      <h1>ログイン</h1>
+      <AuthForm onSubmit={handleSignIn} submitText="ログイン" />
+      <br />
+      <button onClick={() => router.push('/auth/register')}>
+        新規登録はこちら
+      </button>
+    </div>
+  );
+}
