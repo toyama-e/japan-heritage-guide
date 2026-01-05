64a310e94caa0319cd59f5be5f4571489295c549
flyingpoppo <tsubamestar14@gmail.com>
2026-01-05 23:05:43 +0900

    change: importファイルの修正

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.sql import func
-from app.db.base import Base
+# from app.db.base import Base
+from app.core.database import Base
 
 class User(Base):
     __tablename__ = "users"
