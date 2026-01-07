302180983858e457636a5ed698b5d19ba11fe320
flyingpoppo <tsubamestar14@gmail.com>
2026-01-07 21:19:41 +0900

    update: ヘッダーのpadding調整

--- frontend/components/navigation/Header.tsx
+++ frontend/components/navigation/Header.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <div className="header w-105 mx-auto p-5 bg-[#FAFAF7]">
+    <div className="header w-105 mx-auto pt-5 pr-5 pb-0 pl-5 bg-[#FAFAF7]">
       <h1>
         <Link href="/">
           <Image
