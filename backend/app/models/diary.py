d36358e0c59abeca935238a612548c2092b85a45
flyingpoppo <tsubamestar14@gmail.com>
2026-01-11 13:31:12 +0900

    feat: diaryテーブルにいいねカウントカラムを追加

--- backend/app/models/diary.py
+++ backend/app/models/diary.py
@@ -35,6 +35,7 @@ class Diary(Base):
     title = Column(String, nullable=False)
     text = Column(Text, nullable=False)
     image_url = Column(String, nullable=True)
+    like_count = Column(Integer, nullable=False, server_default="0")
     created_at = Column(
         DateTime(timezone=True),
         server_default=func.now(),
